be54abb6641605037670d4d4a27b7516bbea6a5d
rishabh vishwakarma <vishwakarmarishabh378@gmail.com>
2022-06-09 08:20:49 +0530

    intial change in config file

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const db = mongoose.connection;
-mongoose.connect(`mongodb+srv://Rishabh8109:1Rishu8109@er-cluster.hxdx5rg.mongodb.net/E-Reviews`, {useNewUrlParser: true , useUnifiedTopology: true});
+mongoose.connect(`mongodb+srv://Rishabh8109:1Rishu8109@er-cluster.hxdx5rg.mongodb.net/E-Reviews`, {useNewUrlParser: true});
 
 db.on('error' , function(err) {
    console.log("Error in conneting to mongodb");
