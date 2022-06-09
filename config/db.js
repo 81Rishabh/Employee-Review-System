7eb1ca6c2aab2054237e8bbe36e25e91f321f3f3
rishabh vishwakarma <vishwakarmarishabh378@gmail.com>
2022-06-09 08:10:14 +0530

    intial change

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const db = mongoose.connection;
-mongoose.connect(`mongodb+srv://Rishabh8109:${process.env.mongdb_pass}@er-cluster.hxdx5rg.mongodb.net/E-Reviews`, {useNewUrlParser: true , useUnifiedTopology: true});
+mongoose.connect(`mongodb+srv://Rishabh8109:${process.env.MONGODB_PASS}@er-cluster.hxdx5rg.mongodb.net/E-Reviews`, {useNewUrlParser: true , useUnifiedTopology: true});
 
 db.on('error' , function(err) {
    console.log("Error in conneting to mongodb");
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-const dotenv = require('dotenv').config();
-const path = require('path');
+const path = require('path')
+require('dotenv').config();
+
 const sassMiddleware = require('node-sass-middleware');
 const db = require('./config/db');
 const MongoStore = require('connect-mongo');
