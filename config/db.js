e7b049ed3db4efb93353749be21bdde1b4df5c44
rishabh vishwakarma <vishwakarmarishabh378@gmail.com>
2022-06-09 09:07:18 +0530

    intial commit

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const db = mongoose.connection;
-mongoose.connect(`mongodb+srv://Rishabh8109:1Rishu8109@er-cluster.hxdx5rg.mongodb.net/?retryWrites=true&w=majority`);
+mongoose.connect(process.env.MONGODB_URI , {
+  useNewUrlParser: true,
+});
 
 db.on('error' , function(err) {
    console.log("Error in conneting to mongodb");
